fc3674460d09f10ad2893f68b2cc09e85a1e2a6f
Wilbur Liu <wilburliu1123@gmail.com>
2021-04-17 09:57:45 -0500

    ch8

---
+++ chapter8/helloworld.c
@@ -0,0 +1,6 @@
+//#include <sys/types.h>
+#include <unistd.h>
+int main() {
+    write(1, "hello, world\n", 13);
+    _exit(0);
+}
---
+++ chapter8/helloworld.s
@@ -0,0 +1,29 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 11, 0	sdk_version 11, 1
+	.globl	_main                   ; -- Begin function main
+	.p2align	2
+_main:                                  ; @main
+	.cfi_startproc
+; %bb.0:
+	stp	x29, x30, [sp, #-16]!   ; 16-byte Folded Spill
+	mov	x29, sp
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+Lloh0:
+	adrp	x1, l_.str@PAGE
+Lloh1:
+	add	x1, x1, l_.str@PAGEOFF
+	mov	w0, #1
+	mov	w2, #13
+	bl	_write
+	mov	w0, #0
+	bl	__exit
+	.loh AdrpAdd	Lloh0, Lloh1
+	.cfi_endproc
+                                        ; -- End function
+	.section	__TEXT,__cstring,cstring_literals
+l_.str:                                 ; @.str
+	.asciz	"hello, world\n"
+
+.subsections_via_symbols
